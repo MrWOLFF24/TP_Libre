e7cff96c9f8957ce2936550a65ab63c0570d0a21
MrWOLFF24 <kakashi24@live.fr>
2018-02-08 13:44:18 +0100

    tp libre

---
+++ assets/js/app.js
@@ -0,0 +1,36 @@
+const app = (function () {
+        "use strict";
+
+    let bg;
+    let answer;
+
+    function ajaxCall(clbk){
+        const url = 'https://yesno.wtf/api';
+        fetch(url)
+            .then(res => res.json())
+            .then((data) => {
+                clbk(data);
+            });
+    }
+
+    ajaxCall(function(res){
+        console.log("res =>");
+        bg.style.background = `url(${res.image}) center no-repeat`;
+        bg.style.backgroundSize = "cover";
+        answer.textContent = res.answer;
+
+    });
+
+    function btn() {
+        answer.addEventListener("click", function () {
+            window.location.reload();
+        });
+    }
+
+    window.onload = function init() {
+        bg = document.getElementById("api_test");
+        answer = document.getElementById("answer");
+        btn();
+    }
+
+}());
